924f8b2322ebc8822992b14842dc76eacef8a39f
unknown <murodzon104@gmail.com>
2022-02-12 21:26:40 +0500

    Updated script.js

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Infinity Scroll App
+# Infinite Scroll App
 
----659
+This app shows how Infinite Scroll works
+
+## Built With
+
+- HTML
+- CSS
+- JavaScript
+
+## License
+
+Distributed under the [MIT](https://choosealicense.com/licenses/mit/) License.
+
+## Contact
+
+Khasanov Murodjon - murodzon104@gmail.com
+
+Project Link - [https://github.com/mur073/infinite-scroll/](https://github.com/mur073/infinite-scroll/)
---
+++ script.js
@@ -0,0 +1,73 @@
+const container = document.getElementById("container");
+const loader = document.getElementById("loader");
+
+let images = [];
+let loadedImages = 0;
+let totalImages = 0;
+let isReady = false;
+
+const count = 2;
+const apiKey = "zV2xQ6tkb5d2mJQpP03Ns1MvIcv9ayQcVmvCkXWXlgk";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  loadedImages++;
+  if (loadedImages === totalImages) {
+    isReady = true;
+    loader.hidden = true;
+  }
+}
+
+function setAttrs(element, attr) {
+  for (let key in attr) {
+    element.setAttribute(key, attr[key]);
+  }
+}
+
+function displayImages() {
+  loadedImages = 0;
+  totalImages = images.length;
+
+  images.forEach((image) => {
+    const item = document.createElement("a");
+    const img = document.createElement("img");
+
+    setAttrs(item, {
+      href: image.links.html,
+      target: "_blank",
+    });
+
+    setAttrs(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+
+    img.addEventListener("load", imageLoaded);
+
+    item.appendChild(img);
+    container.appendChild(item);
+  });
+}
+
+async function getImages() {
+  try {
+    const response = await fetch(apiUrl);
+    images = await response.json();
+    displayImages();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getImages();
+
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    isReady
+  ) {
+    isReady = false;
+    getImages();
+  }
+});
